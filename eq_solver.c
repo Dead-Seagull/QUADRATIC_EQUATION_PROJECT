a8d9d1431bf049b3b476f10c3e671089c9cec887
Lophophora24 <39420392+Lophophora24@users.noreply.github.com>
2021-09-13 01:54:39 +0300

    Add files via upload

--- eq_solver.c
+++ eq_solver.c
@@ -5,12 +5,48 @@
 
 int eq_solver(double a, double b, double c, double* x1, double* x2);
 void unit_test();
+void single_test();
 
 int main() {
 
+    single_test();
+
+    return 0;
+
+}
+
+int eq_solver(double a, double b, double c, double* x1, double* x2) {
+    if (a == 0) {
+        if (b == 0) {
+            return (c == 0) ? INF_NUM : 0;
+        } else {
+            *x1 = -c / b;
+            return 1;
+        }
+    } else {
+        double d = b*b - 4*a*c;
+        if (d == 0) {
+            *x1 = *x2 = -b / (2.0*a);
+            return 1;
+        } else if (d > 0) {
+            double sqrt_d = sqrt(d);
+            *x1 = (-b - sqrt_d) / (2.0*a);
+            *x2 = (-b + sqrt_d) / (2.0*a);
+            return 2;
+        } else {
+            return 0;
+        }
+    }
+}
+
+void unit_test() {
+    
+}
+
+void single_test() {
     double a = 0, b = 0, c = 0; 
     double x1 = 0, x2 = 0;
-    
+
     int num = 0;
 
     printf("This program solves quadratic equation (ax^2+bx+c=0). Put the a, b, c parametrs into the program...\n");
@@ -39,32 +75,5 @@ int main() {
         break;
     default:
         break;
-    }
-
-    return 0;
-
-}
-
-int eq_solver(double a, double b, double c, double* x1, double* x2) {
-    if (a == 0) {
-        if (b == 0) {
-            return (c == 0) ? INF_NUM : 0;
-        } else {
-            *x1 = -c / b;
-            return 1;
-        }
-    } else {
-        double d = b*b - 4*a*c;
-        if (d == 0) {
-            *x1 = *x2 = -b / (2.0*a);
-            return 1;
-        } else if (d > 0) {
-            double sqrt_d = sqrt(d);
-            *x1 = (-b - sqrt_d) / (2.0*a);
-            *x2 = (-b + sqrt_d) / (2.0*a);
-            return 2;
-        } else {
-            return 0;
-        }
     }
 }
